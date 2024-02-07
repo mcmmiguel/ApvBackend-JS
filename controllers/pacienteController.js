4cb8cd1d89b2cc3d9bedaee9e516adc0ffafd33d
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-06 19:48:25 -0600

    Feat. Update patient

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -23,18 +23,43 @@ export const obtenerPaciente = async (req, res) => {
     const { id } = req.params;
     const paciente = await Paciente.findById(id);
 
+    if (!paciente) {
+        return res.status(404).json({ msg: 'No encontrado' });
+    }
+
     if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
         return res.json({ msg: 'Accion no válida' });
     }
 
-    if (paciente) {
-        res.json(paciente);
-    }
+    res.json(paciente);
 
 }
 
 export const actualizarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
 
+    if (!paciente) {
+        return res.status(404).json({ msg: 'No encontrado' });
+    }
+
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({ msg: 'Accion no válida' });
+    }
+
+    // Actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export const eliminarPaciente = async (req, res) => {
