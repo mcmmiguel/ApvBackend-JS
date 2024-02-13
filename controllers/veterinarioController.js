292832c6dbc3e7c0fd8d8a17c318a52680a43277
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-12 22:00:58 -0600

    Chore. Return different obj

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -82,7 +82,12 @@ export const autenticar = async (req, res) => {
     // Revisar el password
     if (await usuario.comprobarPassword(password)) {
         // Autenticar
-        res.json({ token: generarJWT(usuario.id) })
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario.id),
+        });
     } else {
         const error = new Error('Password incorrecto');
         return res.status(403).json({ msg: error.message });
