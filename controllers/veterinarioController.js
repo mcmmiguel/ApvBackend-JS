524e4ebd0b89560fa872678501a54d9f453b3566
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-13 18:20:01 -0600

    Feat. Update profile

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -158,3 +158,33 @@ export const nuevoPassword = async (req, res) => {
         console.log(error);
     }
 }
+
+export const actualizarPerfil = async (req, res) => {
+    const veterinario = await Veterinario.findById(req.params.id);
+    if (!veterinario) {
+        const error = new Error('Hubo un error');
+        return res.status(400).json({ msg: error.message });
+    }
+
+    const { email } = req.body;
+    if (veterinario.email !== req.body.email) {
+        const existeEmail = await veterinario.findOne({ email });
+
+        if (existeEmail) {
+            const error = new Error('El email ingresado ya se encuentra en uso');
+            return res.status(400).json({ msg: error.message });
+        }
+    }
+
+    try {
+        veterinario.nombre = req.body.nombre;
+        veterinario.email = req.body.email;
+        veterinario.web = req.body.web;
+        veterinario.telefono = req.body.telefono;
+
+        const veterinarioActualizado = await veterinario.save();
+        res.json(veterinarioActualizado);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { autenticar, comprobarToken, confirmar, nuevoPassword, olvidePassword, perfil, registrar } from '../controllers/veterinarioController.js';
+import { autenticar, comprobarToken, confirmar, nuevoPassword, olvidePassword, perfil, registrar, actualizarPerfil } from '../controllers/veterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -14,6 +14,7 @@ router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword)
 
 // Area privada
 router.get('/perfil', checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil);
 // router.get('/perfil', checkAuth, perfil);
 
 export default router;
